cef8b5f3941c2804510a7a76230461ba08e18ec8
eshamster <hamgoostar@gmail.com>
2020-02-23 07:25:34 +0000

    Try to recognize project to ASDF

--- test-docker/test.sh
+++ test-docker/test.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
-set -eux
+# set -eux
+set -ux
 
 lisp=$1
 ros use ${lisp}
 
 # Note: Assume that repository is checkout to workspace folder in previous step
-cd /github/workspace
+dir=/root/.roswell/local-projects/target
+ln -s /github/workspace ${dir}
+cd ${dir}
+rove *.asd
+
+ros run -e '(ql:register-local-projects)' -q
+rove *.asd
+
+cd
+rm -rf ${dir}
+cp -R /github/workspace ${dir}
+cd ${dir}
+rove *.asd
+
+ros run -e '(ql:register-local-projects)' -q
 rove *.asd
