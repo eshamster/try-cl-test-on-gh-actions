5654baa5d0d04995f75a2013963b231a4a8112fb
eshamster <hamgoostar@gmail.com>
2020-02-23 09:43:39 +0000

    Fix: Replace ln by cp
    In ccl-bin, when using symolic link, the test failed by the following
    error.
    
    Error: Error Component "try-cl-test-on-gh-actions" not found

--- test-docker/test.sh
+++ test-docker/test.sh
@@ -8,6 +8,6 @@ ros use ${lisp}
 
 # Note: Assume that repository is checkout to workspace folder in previous step
 dir=/root/.roswell/local-projects/target
-ln -s /github/workspace ${dir}
+cp -R /github/workspace ${dir}
 cd ${dir}
 rove *.asd
