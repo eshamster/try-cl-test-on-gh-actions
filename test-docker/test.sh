bf10cbadf088e25f5a613ed8a2a0827b9ceed020
eshamster <hamgoostar@gmail.com>
2020-02-23 04:15:59 +0000

    Fix current directory in test.sh

--- test-docker/test.sh
+++ test-docker/test.sh
@@ -5,6 +5,6 @@ set -eux
 lisp=$1
 ros use ${lisp}
 
-# Note: Assume that repository is checkout to home folder in previous step
-cd ~
+# Note: Assume that repository is checkout to workspace folder in previous step
+cd /github/workspace
 rove *.asd
