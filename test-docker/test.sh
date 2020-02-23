32dbf2ba88fa43bb431e2edef3c2f4ad914f0dcc
eshamster <hamgoostar@gmail.com>
2020-02-23 04:01:16 +0000

    Add some investigation code

--- test-docker/test.sh
+++ test-docker/test.sh
@@ -6,6 +6,10 @@ lisp=$1
 ros use ${lisp}
 
 find / -name "*.asd"
+echo "--------------------"
+find /github
+echo "--------------------"
+find /
 
 dir=/root/.roswell/local-projects/target
 ln -s ${HOME} ${dir}
