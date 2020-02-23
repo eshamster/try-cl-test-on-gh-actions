79b0fb33d378425f0646a2f992fe7d0393a9dca0
eshamster <hamgoostar@gmail.com>
2020-02-23 04:06:59 +0000

    Add checkout step

--- test-docker/test.sh
+++ test-docker/test.sh
@@ -8,8 +8,6 @@ ros use ${lisp}
 find / -name "*.asd"
 echo "--------------------"
 find /github
-echo "--------------------"
-find /
 
 dir=/root/.roswell/local-projects/target
 ln -s ${HOME} ${dir}
