a35f9f93097b8c6fdcef6768f02f339dae60f68c
eshamster <hamgoostar@gmail.com>
2020-02-24 07:24:21 +0000

    Add installTargets parameter

--- test-docker/test.sh
+++ test-docker/test.sh
@@ -3,6 +3,15 @@
 set -eux
 
 lisp=$1
+install_targets="${2:-}"
+
+if [ "${install_targets}" != "" ]; then
+    echo "${install_targets}" | tr ',' '\n'| while read target; do
+        ros install ${target}
+    done
+    ros -e '(ql:register-local-projects)' -q
+fi
+
 ros use ${lisp}
 
 # Note: Assume that repository is checkout to workspace folder in previous step
@@ -11,5 +20,5 @@ cp -R /github/workspace ${dir}
 cd ${dir}
 rove *.asd 2>&1 | tee /tmp/test.log
 # Note: In Clozure CL, terminating debug console finishes in errcode 0,
-# so grep message to check if the test has run.
+# so grep message to check if the test has actually run.
 grep "test passed" /tmp/test.log
